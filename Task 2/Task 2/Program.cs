5576a9890e83813b413c0b656c8249e9c8b11d37
Zphorium <zulfugarsm@code.edu.az>
2022-06-12 22:17:12 +0400

    Second task uploaded

---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+while (true)
+{
+    Console.Write("Pls enter your data : ");
+    string enteredData = Console.ReadLine();
+    string ReturnDataLower = string.Empty;
+
+    for (int i = 0; i < enteredData.Length; i++)
+    {
+
+        if (enteredData[i] >= 'A' && enteredData[i] <= 'Z')
+        {
+            ReturnDataLower += (char)(enteredData[i] - 'A' + 'a');
+        }
+        else
+        {
+            ReturnDataLower += enteredData[i];
+        }
+    }
+    Console.WriteLine($"Entered data : {ReturnDataLower}");
+}
