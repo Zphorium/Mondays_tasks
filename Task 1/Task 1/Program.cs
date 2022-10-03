f8fd99d771bbe8a61be590d1a7c143526dc7bf4e
Zphorium <106176680+Zphorium@users.noreply.github.com>
2022-10-04 00:02:52 +0400

    tasks are uploaded succesfully

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+while (true)
+{
+    Console.Write("Pls enter your data : ");
+    string enteredData = Console.ReadLine();
+    string ReturnDataUpper = string.Empty;
+
+    for (int i = 0; i < enteredData.Length; i++)
+    {
+
+        if (enteredData[i] >= 'a' && enteredData[i] <= 'z')
+        {
+            ReturnDataUpper +=(char)(enteredData[i]-32);
+        }
+        else
+        {
+            ReturnDataUpper += enteredData[i];
+        }
+    }
+    Console.WriteLine($"Entered data : {ReturnDataUpper}");
+
+}
